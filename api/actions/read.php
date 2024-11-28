81faf6039ffe7f450d68bd0eb6557b2e8fc3e9f1
Alex Deidda <alex.deidda@aledesign.co.uk>
2024-11-28 11:22:41 +0000

    Adding headers on Read action

--- api/actions/read.php
+++ api/actions/read.php
@@ -1,8 +1,8 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header("Content-Type: application/json; charset=UTF-8");
-// header("Access-Control-Allow-Methods: GET");
-// header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include_once '../config/database.php';
 include_once '../class/route.php';
@@ -12,9 +12,8 @@ $route = new Route();
 
 $db = $database->getConnection();
 
-// Get the HTTP method
 $method = $_SERVER['REQUEST_METHOD'];
-// Parse the request
+
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $resource = array_shift($request); // e.g., 'users', 'posts', etc.
 
